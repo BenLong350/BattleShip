fa3427fcfe60adc66deba59d0535d327c1cc78cd
Ben Long <b.k.long.350@gmail.com>
2023-07-13 10:20:12 -0400

    This has updated loops to ensure it runs better

--- Battleship.py.py
+++ Battleship.py.py
@@ -12,22 +12,30 @@ def battleship() :
         print("Hell to daw naw, to daw naw naw naw- no please enter either y or n")
         decision = input("Are you ready to play Battle ship? :(y/n) ")
         
-    row = input("Please guess a row :(0/1/2/3/4) ")
-    if row in ('0', '1', '2', '3', '4'):
-        print("Thank you, now please continue to the next section")
-    else:
-        if row != ('0', '1', '2', '3', '4'):
-            print("Good try sucker now enter either 0/1/2/3/4")
-            row = input("Please guess a row :(0/1/2/3/4) ")
     
-    colm = input("Please guess a column :(0/1/2/3/4) ")
-    if colm in ('0', '1', '2', '3', '4'):
-        print("Thank you")
-    else:
-        if colm != ('0', '1', '2', '3', '4'):
-            print("Good try sucker now enter either 0/1/2/3/4")
-            colm = input("Please guess a column :(0/1/2/3/4) ")
-    print("You guessed row", row, "and column", colm)
+    while True:
+        row = input("Please guess a row :(0/1/2/3/4) ")
+        if row in ('0', '1', '2', '3', '4'):
+            print("Thank you, now please continue to the next section")
+            break
+        else:
+            if row != ('0', '1', '2', '3', '4'):
+                print("Good try sucker now enter either 0/1/2/3/4")
+                row = input("Please guess a row :(0/1/2/3/4) ")
+    
+    while True:
+        colm = input("Please guess a column :(0/1/2/3/4) ")
+        if colm in ('0', '1', '2', '3', '4'):
+            print("Thank you")
+            break
+        else:
+            if colm != ('0', '1', '2', '3', '4'):
+                print("Good try sucker now enter either 0/1/2/3/4")
+                colm = input("Please guess a column :(0/1/2/3/4) ")
+            print("You guessed row", row, "and column", colm)
+    
+    list= [row, colm]
+    print("These are the guesses that have been made: " ,list)
     
             
             
