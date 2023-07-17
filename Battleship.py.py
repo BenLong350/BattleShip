b93863a542efcce6932b620f5b67b53dd63f05bf
Ben Long <b.k.long.350@gmail.com>
2023-07-17 10:46:51 -0400

    Working list and ship placement

--- Battleship.py.py
+++ Battleship.py.py
@@ -5,12 +5,9 @@ def battleship() :
     
     round = 1
     import random
-    row2 = random.randrange(0,5)
-    colm2= random.randrange(0,5)
-
-
-    ship_location = (row2, colm2)
-    #print(ship_location)
+    ship_location = None
+    
+    
 
     def ask():
         decision = input("Are you ready to play Battle ship? :(y/n) ")
@@ -22,12 +19,27 @@ def battleship() :
         else: 
             print("Hell to daw naw, to daw naw naw naw- no please enter either y or n")
             decision = input("Are you ready to play Battle ship? :(y/n) ")
+        decision2 = input("Do you want to the ships location to be random? :(y/n) ")
+        if decision2 == "y":
+            row2 = random.randrange(0,5)
+            colm2 = random.randrange(0,5)
+            ship_location1 = (row2, colm2)
+            print(ship_location1)
+            return ship_location1
+        elif decision2 == "n":
+            row3 = int(input("What row would you like the ship to be placed?: "))
+            colm3 = int(input("What column would you like the ship to be placed?: "))
+            ship_location2 = (row3, colm3)
+            print(ship_location2)
+            return ship_location2
+        else:
+            print("bro wth are you doing just pick a damn row and column :")
+            decision2 = input("Do you want the ships location to be random? :(y/n) ")
         
         
-        
-    ask()
+    ship_location = ask()
 
-
+    
 
     arr = [["O", "O", "O", "O", "O"],
            ["O", "O", "O", "O", "O"], 
@@ -42,6 +54,7 @@ def battleship() :
 
     row = 0
     colm = 0
+    guesslist = []
         
     while True:
         while round < 6 :
@@ -76,18 +89,14 @@ def battleship() :
                 break
         
             list= (row, colm)
-            guess_list = []
-            guess_list.append(list)
-            
-            
-            print(guess_list)
+            guesslist.append(list)
+            print("These are the guesses that have been made so far:", guesslist)
         
             if arr[row][colm] == "O":
                 print("**********")
             else:
                 print("You already chose that coordinate!")
         
-
             if list == ship_location:
                 print("You hit the battleship!")
                 arr[row][colm] = "X"
@@ -108,7 +117,7 @@ def battleship() :
                 arr[row][colm] = "M"
             round +=1
         if round > 6 and list == ship_location:
-            print("Dang you lucky son of a gun you got it. COngrats you win")
+            print("Dang you lucky son of a gun you got it. Congrats you win")
             continue
         else:
             print("You lose sucker MUAH HA HA HA HA HA HA HA HA")
@@ -121,28 +130,5 @@ battleship()
     
             
             
-#use for loop for row and colm guessing- needs to cycle through three times
 
-#if guess = correct row and colm
-    #print("Congraulations, you have won BattleShip")
-#elif guess != correct row and colm and not all 3 guesses have been used
-    #print("You missed my BattleShip, please guess a row and column again")
-    #row = input("Please guess a row :(0/1/2/3/4) ")
-    #if row in ('0', '1', '2', '3', '4'):
-        #print("Thank you, now please continue to the next section")
-    #else:
-        #if row != ('0', '1', '2', '3', '4'):
-            #print("Good try sucker now enter either 0/1/2/3/4")
-            #row = input("Please guess a row :(0/1/2/3/4) ")
-    
-    #colm = input("Please guess a column :(0/1/2/3/4) ")
-    #if colm in ('0', '1', '2', '3', '4'):
-        #print("Thank you")
-    #else:
-        #if colm != ('0', '1', '2', '3', '4'):
-            #print("Good try sucker now enter either 0/1/2/3/4")
-            #colm = input("Please guess a column :(0/1/2/3/4) ")
-#else guess != correct row and colm and all 3 guesses have been used
-    #print("You missed my BattleShip, and you lose hahahahahaha")
-    
 
